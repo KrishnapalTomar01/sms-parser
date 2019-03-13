bfd69decbe0e08948fabd689b2741e3ab519178e
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2019-03-13 11:03:43 +0530

    three features added

--- app/src/main/java/com/example/mahendrasinghtomar/smsparser/IncomingSms.java
+++ app/src/main/java/com/example/mahendrasinghtomar/smsparser/IncomingSms.java
@@ -1,10 +1,15 @@
 package com.example.mahendrasinghtomar.smsparser;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
@@ -12,11 +17,33 @@ import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IncomingSms extends BroadcastReceiver {
 
     // Get the object of SmsManager
     final SmsManager sms = SmsManager.getDefault();
     private GPSTracker gpsTracker;
+
+    public List<Contacts> getList(Context context){
+        List<Contacts> contactList=new ArrayList<>();
+        ContentResolver cr=context.getContentResolver();
+        Cursor cursor=cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
+        if (cursor.getCount()> 0) {
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name=cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+                Contacts info = new Contacts(id,name,cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                contactList.add(info);
+
+                }
+
+            cursor.close();
+        }
+        return contactList;
+    }
 
     public void onReceive(Context context, Intent intent) {
 
@@ -45,7 +72,8 @@ public class IncomingSms extends BroadcastReceiver {
                     Toast toast = Toast.makeText(context,
                             "senderNum: " + senderNum + ", message: " + message, duration);
                     toast.show();
-                    if(message.equals("#101")) {
+
+                    if(message.trim().equals("#101")) {
 
                         double latitude=0,longitude=0;
                         gpsTracker = new GPSTracker(context);
@@ -62,6 +90,24 @@ public class IncomingSms extends BroadcastReceiver {
                         smsBody.append(",");
                         smsBody.append(longitude);
                         smsManager.sendTextMessage(senderNum, null, smsBody.toString(), null, null);
+                    } else if(message.startsWith("#102")){
+
+                        String str = message.substring(5);
+                        str=str.trim();
+                        List <Contacts> contactList=getList(context);
+                        for(Contacts c:contactList){
+                            if(c.getName().equalsIgnoreCase(str)){
+                                SmsManager smsManager = SmsManager.getDefault();
+                                smsManager.sendTextMessage(senderNum, null,"Name: "+c.getName()+"\n"+"Number is: "+c.getPhone(), null, null);
+                                Toast.makeText(context,"Name: "+c.getName()+"\n"+"Number is: "+c.getPhone(),Toast.LENGTH_LONG).show();
+                                break;
+                            }
+                        }
+                    }
+                    else if(message.trim().equals("#103")){
+                        AudioManager am=(AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+                        am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                        Toast.makeText(context,"Audio profile chnaged",Toast.LENGTH_LONG).show();
                     }
                 } // end for loop
             } // bundle is null
--- app/src/main/java/com/example/mahendrasinghtomar/smsparser/MainActivity.java
+++ app/src/main/java/com/example/mahendrasinghtomar/smsparser/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.mahendrasinghtomar.smsparser;
 
+import android.Manifest;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.SEND_SMS)!=PackageManager.PERMISSION_GRANTED ||ContextCompat.checkSelfPermission(getApplicationContext(),android.Manifest.permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS,android.Manifest.permission.READ_PHONE_STATE,android.Manifest.permission.READ_SMS,android.Manifest.permission.READ_CONTACTS}, 101);
+            }
+            NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && !notificationManager.isNotificationPolicyAccessGranted()){
+                Intent intent=new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                startActivity(intent);
             }
         } catch (Exception e){
             e.printStackTrace();
